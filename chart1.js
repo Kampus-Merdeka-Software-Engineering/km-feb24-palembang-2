0714c96217e7cbdb4bdbe23ea115ad377878ae3f
AhsanuQaulan <ahsanuqaulan1208@gmail.com>
2024-06-10 13:33:02 +0800

    third commit

--- chart1.js
+++ chart1.js
@@ -26,6 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
         },
               options: {
                   responsive: true
+                  
               }
           });
       })
